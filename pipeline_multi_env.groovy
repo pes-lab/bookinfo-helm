8d036aa173d1474be5e59be029ebbff3aa604f75
fanding <fanding@cn.ibm.com>
2025-04-09 18:04:21 +0800

    update multi env agent switch

--- pipeline_multi_env.groovy
+++ pipeline_multi_env.groovy
@@ -4,17 +4,20 @@ pipeline {
         choice(name: 'ENVIRONMENT', choices: ['dev', 'uat', 'stg'], description: '选择运行环境')
     }  
     agent {
-        node { label 
-            switch(parameters.ENVIRONMENT) {
-                case 'dev':
-                    return 'slave_dev'
-                case 'uat':
-                    return 'slave_uat'
-                case 'stg':
-                    return 'slave_stg'
-                default: 
-                    return 'slave'
-            }}  
+        node { 
+            label {
+                switch(parameters.ENVIRONMENT) {
+                    case 'dev':
+                        return 'slave_dev'
+                    case 'uat':
+                        return 'slave_uat'
+                    case 'stg':
+                        return 'slave_stg'
+                    default: 
+                        return 'slave'
+                }
+            }
+        }  
     }
     stages {
         stage('run pipeline script') {
