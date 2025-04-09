e74cce5bb91f30bad6ac76673534590924760fd8
fanding <fanding@cn.ibm.com>
2025-04-09 15:51:57 +0800

    debug

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,7 +6,9 @@ pipeline {
     stages {
         stage('checkout') {
             steps {
+                sh 'pwd'
                 git 'https://github.com/pes-lab/bookinfo-helm.git'
+                sh 'pwd'
             }
         }
         stage('run pipeline script') {
